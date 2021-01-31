8e9b389b22e8985dc7990d01708be59dee894ee6
Jesus Mira Aldao <jesus.mira1@hotmail.com>
2021-01-31 12:09:03 +0100

    Update to send notifications

--- function/ServiceBusQueueTrigger1/__init__.py
+++ function/ServiceBusQueueTrigger1/__init__.py
@@ -6,6 +6,20 @@ from datetime import datetime
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+ADMIN_EMAIL_ADDRESS = 'jesus.mira1@hotmail.com'
+SENDGRID_API_KEY = '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'
+
+def send_email(email, subject, body):
+    message = Mail(
+        from_email=ADMIN_EMAIL_ADDRESS,
+        to_emails=email,
+        subject=subject,
+        plain_text_content=body)
+    print('Create SendGridAPIClient')
+    sg = SendGridAPIClient(SENDGRID_API_KEY)
+    sg.send(message)
+
+
 def main(msg: func.ServiceBusMessage):
 
     notification_id = int(msg.get_body().decode('utf-8'))
@@ -22,20 +36,56 @@ def main(msg: func.ServiceBusMessage):
     # Construct connection string
     conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
     conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
     print("Connection established")
+    print("Notification ID:" + str(notification_id))
 
     try:
         # TODO: Get notification message and subject from database using the notification_id
+        query_notification = '''SELECT subject, message 
+                                FROM notification
+                                WHERE id = %s;'''
+
+        cur.execute(query_notification, (notification_id,))
+        row = cur.fetchone()
+        subject = row[0]
+        message = row[1]
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
 
         # TODO: Get attendees email and name
-
+        cur.execute('''SELECT first_name, last_name, email 
+                       FROM attendee;''')
+        attendees = cur.fetchall()
+        print(attendees)
         # TODO: Loop through each attendee and send an email with a personalized subject
+        for attendee in attendees:
+            print('{}, {}, {}'.format({'jesus.mira1@hotmail.com'}, {attendee[2]}, {query_notification}))
+            email = attendee[2]
+            custom_subject = subject
+            send_email(email, custom_subject, message)
 
         # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
         print("The Function has executed correctly")
+        notification_date = datetime.utcnow()
+        status = 'Notified {} attendees'.format(len(attendees))
+
+        query_update = '''UPDATE notification 
+                          SET completed_date = %s, status = %s 
+                          WHERE id = %s;'''
+        cur.execute(query_update, (notification_date, status, notification_id))
+        conn.commit()
+        #count = cursor.rowcount
+        print("Record Updated successfully ")
 
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
     finally:
         # TODO: Close connection
+        if(conn):
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
         print("Sentence finally")
