aab369abbf38ac943cd41c274b0a4ac4c21d20dc
Jesus Mira Aldao <jesus.mira1@hotmail.com>
2021-02-02 12:43:58 +0100

    Protect KEYS and PASSWORDS in webapp

--- web/config.py
+++ web/config.py
@@ -6,13 +6,13 @@ class BaseConfig:
     DEBUG = True
     POSTGRES_URL="postgresql-server-jmiraal.postgres.database.azure.com"
     POSTGRES_USER="jmiraal@postgresql-server-jmiraal"
-    POSTGRES_PW="Vendetta41!"
+    POSTGRES_PW=os.environ['POSTGRES_PW']
     POSTGRES_DB="techconfdb"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DR81U1f+sVmmBQRYW2a+PSxcIG3J0apdlovg+SjUweA='
+    SECRET_KEY=os.environ['SECRET_KEY']
+    SERVICE_BUS_CONNECTION_STRING =os.environ['SERVICE_BUS_CONNECTION_STRING']
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'jesus.mira1@hotmail.com'
     SENDGRID_API_KEY = ''#Configuration not required, required SendGrid Account
